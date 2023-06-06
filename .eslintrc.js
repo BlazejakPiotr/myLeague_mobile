7a15b6538d0a9ae59ae08e9d1fe82adb84da8828
Piotr Blazejak <piotr.blazejak@yahoo.com>
2023-06-06 18:55:18 +0200

    set up eslint & fix compile error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
+  extends: ['@react-native-community'],
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'react-native/no-unused-styles': 1,
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 1,
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.ios.jsx',
-          '.android.jsx',
-          '.js',
-          '.jsx',
-          '.json',
-          '.ts',
-          '.tsx',
-        ],
-        root: ['.'],
-        alias: {
-          '@src': './src',
-        },
-      },
-    ],
-  ],
+  plugins: ['react-native-reanimated/plugin'],
 };
