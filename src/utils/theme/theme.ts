b85bba0e6dcc8928c291a63cd466508d1fe99ec4
Piotr Blazejak <piotr.blazejak@yahoo.com>
2023-06-06 16:31:53 +0200

    added app theme

---
+++ src/utils/theme/theme.ts
@@ -0,0 +1,30 @@
+export const colors = {
+  grey100: '#A09B8C',
+  grey150: '#5B5A56',
+  grey200: '#3C3C41',
+  grey300: '#1E2328',
+  greyCool: '#1E282D',
+  hextechBlack: '#010A13',
+  gold100: '#F0E6D2',
+  gold200: '#C8AA6E',
+  gold300: '#C8AA6E',
+  gold400: '#C8983C',
+  gold500: '#785A28',
+  gold600: '#463714',
+  gold700: '#32281E',
+  blue100: '#CDFAFA',
+  blue200: '#0AC8B9',
+  blue300: '#0397AB',
+  blue400: '#005A82',
+  blue500: '#0A323C',
+  blue600: '#091428',
+  blue700: '#0A1428',
+};
+
+export const getColors = (colorName: keyof typeof colors) => {
+  const currentColor = colors[colorName];
+  if (!currentColor) {
+    throw new Error(`Color ${colorName} is not defined`);
+  }
+  return colors[colorName];
+};
