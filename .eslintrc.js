556a7f00f97d6b02477749040a3739c148eff9bd
Piotr Blazejak <piotr.blazejak@yahoo.com>
2023-06-10 16:57:07 +0200

    Splash Screen

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,10 @@ module.exports = {
   rules: {
     'react-native/no-inline-styles': 0,
     'react-native/no-unused-styles': 1,
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
     'no-console': 1,
     '@typescript-eslint/no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
   },
 };
