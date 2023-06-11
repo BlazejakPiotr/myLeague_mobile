2cdab121e3a1ee45b4028a62bf20b0cc603fa7c1
Piotr Blazejak <piotr.blazejak@yahoo.com>
2023-06-12 00:17:16 +0200

    created constants & config

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+const environment = 'development';
+
+const defaultConfig = {
+  currentAppVersion: 1,
+  appStoreLink: 'https://',
+  playStoreLink: 'https://',
+};
+
+const configs = {
+  development: {
+    ...defaultConfig,
+    dataDragonUrl: 'https://ddragon.leagueoflegends.com/api/',
+    riotUrl: 'api.riotgames.com/lol/',
+  },
+};
+
+export const config = configs[environment];
