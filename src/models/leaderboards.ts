fffcf8b47d3821b0b837a62d0cdd069e451f863a
Piotr Blazejak <piotr.blazejak@yahoo.com>
2023-06-22 01:23:52 +0200

    ranked model

---
+++ src/models/leaderboards.ts
@@ -0,0 +1,25 @@
+export interface IRankedResults {
+  leagueId: string;
+  queueType: string;
+  tier: string;
+  rank: IRanks;
+  summonerId: string;
+  summonerName: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  veteran: boolean;
+  inactive: boolean;
+  freshBlood: boolean;
+  hotStreak: boolean;
+}
+
+export type IRanks =
+  | 'BRONZE'
+  | 'SILVER'
+  | 'GOLD'
+  | 'PLATINUM'
+  | 'DIAMOND'
+  | 'MASTER'
+  | 'GRANDMASTER'
+  | 'CHALLENGER';
