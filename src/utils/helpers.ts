ab8b7281cd4f6039ac2f504b9c7b625ef82c7b85
Piotr Blazejak <piotr.blazejak@yahoo.com>
2023-06-22 01:18:22 +0200

    added ranked emblembs

---
+++ src/utils/helpers.ts
@@ -0,0 +1,10 @@
+export const getRankedEmblems = {
+  BRONZE: require('../assets/img/ranked-emblems/emblem-bronze.png'),
+  SILVER: require('../assets/img/ranked-emblems/emblem-silver.png'),
+  GOLD: require('../assets/img/ranked-emblems/emblem-gold.png'),
+  PLATINUM: require('../assets/img/ranked-emblems/emblem-platinum.png'),
+  DIAMOND: require('../assets/img/ranked-emblems/emblem-diamond.png'),
+  MASTER: require('../assets/img/ranked-emblems/emblem-master.png'),
+  GRANDMASTER: require('../assets/img/ranked-emblems/emblem-grandmaster.png'),
+  CHALLENGER: require('../assets/img/ranked-emblems/emblem-challenger.png'),
+};
